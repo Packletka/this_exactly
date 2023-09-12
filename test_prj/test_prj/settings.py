bc23575515eea605994f14b118541b7b038a4d6d
Legion <pavel.azyava@gmail.com>
2023-09-12 21:43:20 +0300

    paxyiy

--- test_prj/test_prj/settings.py
+++ test_prj/test_prj/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 STATIC_URL = 'static_news/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static_news"
+    BASE_DIR / "static",
+    '/var/www/static/'
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
